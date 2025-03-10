c4bfed55be41aea596c7b08994caf021aeccfe20
king_0417 <73704053+gywns0417@users.noreply.github.com>
2025-03-10 22:11:56 +0900

    feat: 회원 엔티티 제약조건 추가 및 회원 생성 시점 필요한 데이터 수정

--- src/main/java/org/ject/support/domain/member/dto/MemberDto.java
+++ src/main/java/org/ject/support/domain/member/dto/MemberDto.java
@@ -9,22 +9,25 @@ public class MemberDto {
 
     public record RegisterRequest (
 
-            @NotBlank @Pattern(regexp = "^[가-힣]{1,5}$", message = "한글 1~5글자만 입력 가능합니다.")
-            String name,
-
-            @NotBlank @Pattern(regexp = "^010\\d{8}$", message = "010으로 시작하는 11자리 숫자를 입력하세요.")
-            String phoneNumber,
             @NotBlank @Pattern(regexp = "^\\d{6}$", message = "PIN 번호는 6자리 숫자여야 합니다.") String pin) {
 
         public Member toEntity(String email, String encodedPin) {
             return Member.builder()
-                    .name(name)
-                    .phoneNumber(phoneNumber)
                     .email(email)
                     .pin(encodedPin)
                     .role(Role.TEMP)
                     .build();
         }
+    }
+
+    public record updateMemberRequest (
+
+            @NotBlank @Pattern(regexp = "^[가-힣]{1,5}$", message = "한글 1~5글자만 입력 가능합니다.")
+            String name,
+
+            @NotBlank @Pattern(regexp = "^010\\d{8}$", message = "010으로 시작하는 11자리 숫자를 입력하세요.")
+            String phoneNumber) {
+
     }
 
     public record RegisterResponse(String accessToken, String refreshToken) {
