f471773c64064ed292bf7852ec72dc147a5635b0
king_0417 <73704053+gywns0417@users.noreply.github.com>
2025-03-14 02:42:34 +0900

    feat: 리프레시 토큰 재발급 관련 DTO 작성

--- src/main/java/org/ject/support/domain/auth/AuthDto.java
+++ src/main/java/org/ject/support/domain/auth/AuthDto.java
@@ -9,4 +9,12 @@ public class AuthDto {
     public record VerifyAuthCodeOnlyResponse(String verificationToken) {
         // 인증번호 검증 성공 시 발급되는 임시 토큰
     }
+    
+    public record TokenRefreshRequest(String refreshToken) {
+        // 토큰 재발급 요청 DTO
+    }
+    
+    public record TokenRefreshResponse(String accessToken) {
+        // 토큰 재발급 응답 DTO
+    }
 }
