c8687095c3e443d772fce0ee1093307c3daf8db0
YeaChan05 <qkenrdl05@gmail.com>
2025-03-23 14:27:15 +0900

    refactor: simplify PageResponse structure and improve pagination handling

--- src/main/java/org/ject/support/common/data/PageResponse.java
+++ src/main/java/org/ject/support/common/data/PageResponse.java
@@ -1,31 +1,23 @@
 package org.ject.support.common.data;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
 
-@JsonIgnoreProperties(ignoreUnknown = true, value = {"pageable", "sort", "empty", "first"})
-public class PageResponse<E> extends PageImpl<E> {
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public final class PageResponse<E> {
+    private final boolean hasNext;
+    private final List<E> items;
+    private final int pageNumber;
+    private final int pageSize;
+    private final int totalPages;
+    private final long totalElements;
+    private final boolean isLast;
 
-    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-    private PageResponse(@JsonProperty("content") List<E> content,
-                         @JsonProperty("number") int page,
-                         @JsonProperty("size") int size,
-                         @JsonProperty("totalElements") long total) {
-        super(content, PageRequest.of(page, size), total);
-    }
-
-    public static <E> PageResponse<E> from(final List<E> content, Pageable pageable, long total) {
-        return new PageResponse<>(content, pageable.getPageNumber(), pageable.getPageSize(), total);
-    }
-
-    @JsonProperty("hasNext")
-    public boolean hasNext() {
-        return super.hasNext();
+    public static <E> PageResponse<E> from(final Page<E> page) {
+        return new PageResponse<>(page.hasNext(), page.getContent(), page.getNumber(), page.getSize(), page.getTotalPages(), page.getTotalElements(), page.isLast());
     }
 }
