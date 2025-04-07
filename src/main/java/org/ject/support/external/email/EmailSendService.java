52ebf2177eba9ef02b6fb8d81c14ffbbbb6338a6
king_0417 <73704053+gywns0417@users.noreply.github.com>
2025-04-07 22:28:22 +0900

    chore: 이메일 발송 기능 가상 스레드 적용

--- src/main/java/org/ject/support/external/email/EmailSendService.java
+++ src/main/java/org/ject/support/external/email/EmailSendService.java
@@ -1,13 +1,10 @@
 package org.ject.support.external.email;
 
-import java.security.SecureRandom;
-import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ject.support.common.util.Json2MapSerializer;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
@@ -39,12 +36,14 @@ public class EmailSendService {
     }
 
     private void sendMail(final MimeMessagePreparator ...preparators) {
-        try {
-            mailSender.send(preparators);
-        } catch (MailSendException e) {
+        Thread thread = Thread.startVirtualThread(() -> mailSender.send(preparators));
+        thread.setUncaughtExceptionHandler((t, e) -> {
+            log.error("가상 스레드에서 메일 전송 실패 {}: {}", t.getName(), e.getMessage(), e);
             throw new MailSendException(MailErrorCode.MAIL_SEND_FAILURE);
-        }
+        });
     }
+
+
 
     public record MailSendRequest(
             String to,
