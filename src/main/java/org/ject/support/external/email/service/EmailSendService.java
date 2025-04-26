021c043a34e40cc22c97e2fc5c159170fee81d7c
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-04-27 02:49:28 +0900

    feat: sendEmail 메서드에 각 코드마다 주석 추가

--- src/main/java/org/ject/support/external/email/service/EmailSendService.java
+++ src/main/java/org/ject/support/external/email/service/EmailSendService.java
@@ -27,25 +27,30 @@ public class EmailSendService {
      * 단건 email 전송
      */
     public void sendEmail(String to, EmailTemplate emailTemplate, Map<String, String> parameter) {
+        // 수신자(destination) 설정
         Destination destination = Destination.builder()
                 .toAddresses(to)
                 .build();
 
+        // 사용할 템플릿 선택 및 변수 매핑
         Template template = Template.builder()
                 .templateName(emailTemplate.getTemplateName())
                 .templateData(map2JsonSerializer.serializeAsString(parameter))
                 .build();
 
+        // 이메일 콘텐츠 설정
         EmailContent emailContent = EmailContent.builder()
                 .template(template)
                 .build();
 
+        // 이메일 요청 객체 생성
         SendEmailRequest emailRequest = SendEmailRequest.builder()
                 .destination(destination)
                 .content(emailContent)
                 .fromEmailAddress(from)
                 .build();
 
+        // AWS SES를 통해 이메일 발송 요청
         sesV2Client.sendEmail(emailRequest);
     }
 }
