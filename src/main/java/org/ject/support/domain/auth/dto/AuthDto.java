73e3bece34406dc5f90421cb9b3654ef180f178e
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-08-18 01:14:01 +0900

    feat: 인증 정보 hidden 처리

--- src/main/java/org/ject/support/domain/auth/dto/AuthDto.java
+++ src/main/java/org/ject/support/domain/auth/dto/AuthDto.java
@@ -1,5 +1,6 @@
 package org.ject.support.domain.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -9,13 +10,14 @@ public class AuthDto {
     public record VerifyAuthCodeRequest(String email, String authCode) {
         // 이메일 인증 코드 검증 요청 DTO
     }
-    
+
+    @Schema(hidden = true)
     public record PinLoginRequest(
-        @NotBlank @Email String email,
-        @NotBlank @Pattern(regexp = "^\\d{6}$", message = "PIN 번호는 6자리 숫자여야 합니다.") String pin) {
+            @NotBlank @Email String email,
+            @NotBlank @Pattern(regexp = "^\\d{6}$", message = "PIN 번호는 6자리 숫자여야 합니다.") String pin) {
         // PIN 로그인 요청 DTO
     }
-    
+
     public record TokenRefreshRequest(String refreshToken) {
         // 토큰 재발급 요청 DTO
     }
