be9c6fdd49e2ea80b3b03984dc05e0bf113212bf
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-08-18 01:08:33 +0900

    refactor: 미존재 ErrorCode enum 시 렌더링 에러 대신 unknown example 명시

--- src/main/java/org/ject/support/common/springdoc/ErrorResponseCustomizer.java
+++ src/main/java/org/ject/support/common/springdoc/ErrorResponseCustomizer.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 import org.ject.support.common.exception.ErrorCode;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.stereotype.Component;
@@ -17,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Component
 public class ErrorResponseCustomizer implements OperationCustomizer {
 
@@ -38,21 +40,39 @@ public class ErrorResponseCustomizer implements OperationCustomizer {
         return Arrays.stream(apiErrorResponses)
                 .map(apiErrorResponse -> {
                     ErrorCode errorCode = getErrorCode(apiErrorResponse);
-                    Example example = new Example();
-                    example.value(new org.ject.support.common.response.ApiResponse<>(errorCode.getCode(), errorCode.getMessage()));
-                    example.description(apiErrorResponse.description());
-                    return ExampleHolder.builder()
-                            .example(example)
-                            .code(apiErrorResponse.code())
-                            .name(apiErrorResponse.name())
-                            .build();
+                    return createExampleHolder(apiErrorResponse, errorCode);
                 })
                 .collect(Collectors.groupingBy(ExampleHolder::code));
     }
 
     @SuppressWarnings("unchecked")
     private <E extends Enum<E> & ErrorCode> E getErrorCode(ApiErrorResponse annotation) {
-        return Enum.valueOf((Class<E>) annotation.value(), annotation.name());
+        try {
+            return Enum.valueOf((Class<E>) annotation.value(), annotation.name());
+        } catch (IllegalArgumentException ex) {
+            log.warn("Unknown ErrorCode enum constant: {}#{}", annotation.value(), annotation.name());
+            return null;
+        }
+    }
+
+    private ExampleHolder createExampleHolder(ApiErrorResponse apiErrorResponse, ErrorCode errorCode) {
+        Example example = new Example();
+        if (errorCode == null) {
+            example.value(null);
+            example.description("'%s' is unknown".formatted(apiErrorResponse.name()));
+            return ExampleHolder.builder()
+                    .example(example)
+                    .code(0)
+                    .name(apiErrorResponse.name())
+                    .build();
+        }
+        example.value(new org.ject.support.common.response.ApiResponse<>(errorCode.getCode(), errorCode.getMessage()));
+        example.description(apiErrorResponse.description());
+        return ExampleHolder.builder()
+                .example(example)
+                .code(apiErrorResponse.code())
+                .name(apiErrorResponse.name())
+                .build();
     }
 
     private void addExampleHoldersToResponses(ApiResponses responses, Map<Integer, List<ExampleHolder>> exampleHolders) {
