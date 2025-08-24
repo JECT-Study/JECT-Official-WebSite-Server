f865aa58093c7eec68c8501b849f03a0ccc9edac
fad <ojy9406@naver.com>
2025-08-24 21:00:48 +0900

    refactor: 랜덤 코드 만들기 숫자, 숫자 + 영문 로직 분리

--- src/test/java/org/ject/support/common/util/CodeGeneratorUtilTest.java
+++ src/test/java/org/ject/support/common/util/CodeGeneratorUtilTest.java
@@ -5,20 +5,41 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CodeGeneratorUtilTest {
 
     @Test
-    @DisplayName("랜덤 인증 코드 생성 테스트")
-    void generateAuthCode_length_check() {
+    @DisplayName("랜덤 인증 숫자 코드 생성 테스트")
+    void generateDigitCode_length_check() {
         // given
         int length = 6;
 
         // when
-        String code = CodeGeneratorUtil.generateAuthCode(length);
+        String code = CodeGeneratorUtil.generateDigitCode(length);
 
         // then
         assertNotNull(code);
         assertEquals(length, code.length());
+        assertTrue(code.matches("[0-9]+"));
+    }
+
+    @Test
+    @DisplayName("랜덤 인증 코드(숫자+대문자) 생성 테스트")
+    void generateAlphaNumCode_length_check() {
+        // given
+        int length = 6;
+
+        // when
+        String code = CodeGeneratorUtil.generateAlphaNumCode(length);
+
+        for (int i = 0; i < 50; i++) {
+            System.out.println(CodeGeneratorUtil.generateAlphaNumCode(length));
+        }
+
+        // then
+        assertNotNull(code);
+        assertEquals(length, code.length());
+        assertTrue(code.matches("[0-9A-Z]+"));
     }
 }
