99c7355c1c14a346b7c772295e138d286c7c5921
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-08-27 01:51:18 +0900

    fix: admin error code 구조 변경

--- src/main/java/org/ject/support/domain/admin/exception/AdminErrorCode.java
+++ src/main/java/org/ject/support/domain/admin/exception/AdminErrorCode.java
@@ -3,13 +3,17 @@ package org.ject.support.domain.admin.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.ject.support.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @AllArgsConstructor
 public enum AdminErrorCode implements ErrorCode {
-    NOT_FOUND_ADMIN("NOT_FOUND_ADMIN", "존재하지 않는 관리자 입니다."),
-    TOO_MANY_REQUESTS("TOO_MANY_REQUESTS", "너무 많은 요청입니다. 잠시 후 다시 시도해주세요.");
+    NOT_FOUND_ADMIN(NOT_FOUND, "NOT_FOUND_ADMIN", "존재하지 않는 관리자 입니다."),
+    TOO_MANY_REQUESTS(HttpStatus.TOO_MANY_REQUESTS, "TOO_MANY_REQUESTS", "너무 많은 요청입니다. 잠시 후 다시 시도해주세요.");
 
+    private final HttpStatus httpStatus;
     private final String code;
     private final String message;
 }
