06be36b44d37664dd6cf5a6ca49b0abaaabacf05
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-08-26 15:29:17 +0900

    Swagger core/admin API 그룹화 (#253)
    * chore: swagger api 명세를 core와 admin으로 그룹화
    
    * refactor: recruit, semester 어드민 API의 URL 앞에 '/admin' 추가
    
    * refactor: 템플릿 메일 전송 API의 URL 앞에 '/admin' 추가

--- src/main/java/org/ject/support/external/email/controller/ManualEmailSendController.java
+++ src/main/java/org/ject/support/external/email/controller/ManualEmailSendController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/emails/send/manual")
+@RequestMapping("/admin/emails/send/manual")
 @RequiredArgsConstructor
 public class ManualEmailSendController implements ManualEmailSendApiSpec {
 
