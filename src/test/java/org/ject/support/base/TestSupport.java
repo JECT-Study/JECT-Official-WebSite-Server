73c4a54499fbb5f5c999f9b71ef4f0315b01e785
023-dev <0_2_3@naver.com>
2025-09-01 21:03:19 +0900

    refactor: 테스트 지원용 추상 클래스 추가(257)

---
+++ src/test/java/org/ject/support/base/TestSupport.java
@@ -0,0 +1,10 @@
+package org.ject.support.base;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public abstract class TestSupport {
+}
