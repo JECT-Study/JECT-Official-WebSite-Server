8114bb08c36de758b75a44d820cabf39381965cf
fad <ojy9406@naver.com>
2025-09-03 18:54:49 +0900

    fix: Slack Rate Limiter 값 조정

--- src/main/java/org/ject/support/external/infrastructure/SlackRateLimiter.java
+++ src/main/java/org/ject/support/external/infrastructure/SlackRateLimiter.java
@@ -9,13 +9,15 @@ import java.time.Duration;
 @Component
 public class SlackRateLimiter {
 
-    private static final int RATE_LIMIT_PER_SECOND = 1;
+    private static final int MAX_TOKENS = 5;
+    private static final int RATE_LIMIT_PER_SECOND = 5;
+    private static final int REFILL_DURATION_SECONDS = 10;
     private final Bucket bucket;
 
     public SlackRateLimiter() {
         Bandwidth limit = Bandwidth.builder()
-                .capacity(RATE_LIMIT_PER_SECOND)
-                .refillGreedy(RATE_LIMIT_PER_SECOND, Duration.ofSeconds(1))
+                .capacity(MAX_TOKENS)
+                .refillGreedy(RATE_LIMIT_PER_SECOND, Duration.ofSeconds(REFILL_DURATION_SECONDS))
                 .build();
 
         this.bucket = Bucket.builder()
@@ -23,7 +25,7 @@ public class SlackRateLimiter {
                 .build();
     }
 
-    // 반환값이 boolean: 토큰이 있으면 true, 없으면 false
+    // 반환값 boolean: 토큰이 있으면 true, 없으면 false
     public boolean tryConsume(int permits) {
         return bucket.tryConsume(permits);
     }
