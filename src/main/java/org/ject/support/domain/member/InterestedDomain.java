87b1c994840d49375a4ceb196a96afea9db20843
023-dev <0_2_3@naver.com>
2025-10-14 20:34:55 +0900

    refactor: 개선된 프로필 작성에 따른 직군 및 관심 도메인 선택 enum 클래스 리팩토링

---
+++ src/main/java/org/ject/support/domain/member/InterestedDomain.java
@@ -0,0 +1,29 @@
+package org.ject.support.domain.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum InterestedDomain {
+    GAME("게임"),
+    EDUCATION("교육"),
+    MARKETING("모빌리티"),
+    PRODUCTIVITY("생산성"),
+    SOCIAL_NETWORK("소셜 네트워크"),
+    UTILITY("유틸리티"),
+    E_COMMERCE("이커머스"),
+    COMMUNITY("커뮤니티"),
+    CONTENTS("콘텐츠"),
+    TRAVELTECH("트래블테크"),
+    FASHION_BEAUTY("패션/뷰티"),
+    FOODTECH("푸드테크"),
+    PROPTECH("프롭테크"),
+    FINTECH("핀테크"),
+    HEALTHCARE("헬스케어"),
+    HR("HR"),
+    ;
+
+    private final String description;
+
+}
