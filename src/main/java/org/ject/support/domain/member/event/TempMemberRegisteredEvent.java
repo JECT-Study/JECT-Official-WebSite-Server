a4d385813cad023551697f0deaa279e4e3a1fd20
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-10-19 21:24:28 +0900

    clean: TempMemberRegisteredEvent 제거

--- src/main/java/org/ject/support/domain/member/event/TempMemberRegisteredEvent.java
+++
@@ -1,9 +0,0 @@
-package org.ject.support.domain.member.event;
-
-import org.ject.support.domain.member.entity.Member;
-
-public record TempMemberRegisteredEvent(Long memberId) {
-    public static TempMemberRegisteredEvent of(Member member) {
-        return new TempMemberRegisteredEvent(member.getId());
-    }
-}
