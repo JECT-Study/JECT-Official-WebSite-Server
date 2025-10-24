fc4d70d30d8bbea5ed34b0886e2bd20866619220
023-dev <0_2_3@naver.com>
2025-10-24 19:49:04 +0900

    feat: 제출된 지원서 수 조회 API 스펙 추가

--- src/main/java/org/ject/support/domain/admin/controller/SubmittedApplyApiSpec.java
+++ src/main/java/org/ject/support/domain/admin/controller/SubmittedApplyApiSpec.java
@@ -4,11 +4,17 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.ject.support.domain.admin.dto.SubmittedApplyBulkDeleteRequest;
+import org.ject.support.domain.admin.dto.SubmittedApplyCountResponse;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Submitted Apply", description = "제출된 지원서 관리 API")
 public interface SubmittedApplyApiSpec {
+
+    @Operation(
+            summary = "제출된 지원서 수 조회",
+            description = "제출된 상태의 지원서 총 개수를 조회합니다.")
+    SubmittedApplyCountResponse getSubmittedApplyCount();
 
     @Operation(
             summary = "제출된 지원서 삭제",
