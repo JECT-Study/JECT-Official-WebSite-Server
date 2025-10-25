b5d41a53c3e3f5cb1e274d35e7305e095f84c8c6
023-dev <0_2_3@naver.com>
2025-10-25 13:36:03 +0900

    feat: 제출된 지원서 목록 조회 API 스펙 구현

--- src/main/java/org/ject/support/domain/admin/controller/SubmittedApplyApiSpec.java
+++ src/main/java/org/ject/support/domain/admin/controller/SubmittedApplyApiSpec.java
@@ -5,8 +5,14 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.ject.support.domain.admin.dto.SubmittedApplyBulkDeleteRequest;
 import org.ject.support.domain.admin.dto.SubmittedApplyCountResponse;
+import org.ject.support.domain.admin.dto.SubmittedApplyResponse;
+import org.ject.support.domain.member.JobFamily;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "Submitted Apply", description = "제출된 지원서 관리 API")
 public interface SubmittedApplyApiSpec {
@@ -15,6 +21,12 @@ public interface SubmittedApplyApiSpec {
             summary = "제출된 지원서 수 조회",
             description = "제출된 상태의 지원서 총 개수를 조회합니다.")
     SubmittedApplyCountResponse getSubmittedApplyCount();
+
+    @Operation(
+            summary = "제출된 지원서 목록 조회",
+            description = "제출된 지원서들의 목록을 조회합니다.")
+    Page<SubmittedApplyResponse> findSubmittedApplies(@RequestParam(required = false) final JobFamily jobFamily,
+                                                      @PageableDefault(size = 15) final Pageable pageable);
 
     @Operation(
             summary = "제출된 지원서 삭제",
