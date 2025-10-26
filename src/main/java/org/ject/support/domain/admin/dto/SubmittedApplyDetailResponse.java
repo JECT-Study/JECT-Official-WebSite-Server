48a598fb71a22422b978cf41231052595f7794c7
023-dev <0_2_3@naver.com>
2025-10-26 21:24:34 +0900

    feat: 제출된 지원서 상세 조회 DTO 추가

---
+++ src/main/java/org/ject/support/domain/admin/dto/SubmittedApplyDetailResponse.java
@@ -0,0 +1,34 @@
+package org.ject.support.domain.admin.dto;
+
+import java.util.List;
+import java.util.Map;
+import org.ject.support.common.util.DateTimeUtil;
+import org.ject.support.domain.apply.domain.Apply;
+import org.ject.support.domain.apply.dto.ApplyPortfolioDto;
+import org.ject.support.domain.member.JobFamily;
+
+public record SubmittedApplyDetailResponse(
+        Long applyId,
+        String name,
+        String phoneNumber,
+        String email,
+        JobFamily jobFamily,
+        String createdAt,
+        String updatedAt,
+        ApplicationFormResponse applicationFormResponse
+) {
+    public static SubmittedApplyDetailResponse from(Apply apply,
+                                                    Map<String, String> answers,
+                                                    List<ApplyPortfolioDto> portfolios) {
+        return new SubmittedApplyDetailResponse(
+                apply.getId(),
+                apply.getMember().getName(),
+                apply.getMember().getPhoneNumber(),
+                apply.getMember().getEmail(),
+                apply.getMember().getJobFamily(), // apply.getRecruit().getJobFamily(),
+                DateTimeUtil.formatWithDayOfWeek(apply.getCreatedAt()),
+                DateTimeUtil.formatWithDayOfWeek(apply.getUpdatedAt()),
+                ApplicationFormResponse.from(answers, portfolios)
+        );
+    }
+}
