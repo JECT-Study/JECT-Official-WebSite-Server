829b661c79222d04d1193a83c461404f3d13ad7c
023-dev <0_2_3@naver.com>
2025-10-30 23:47:43 +0900

    refactor: File 도메인 에러 코드를 문자열 형태로 수정

--- src/main/java/org/ject/support/domain/file/exception/FileErrorCode.java
+++ src/main/java/org/ject/support/domain/file/exception/FileErrorCode.java
@@ -11,8 +11,8 @@ import static org.springframework.http.HttpStatus.PAYLOAD_TOO_LARGE;
 @Getter
 @AllArgsConstructor
 public enum FileErrorCode implements ErrorCode {
-    INVALID_EXTENSION(BAD_REQUEST, "INVALID_EXTENSION", "유효하지 않은 확장자입니다."),
-    EXCEEDED_PORTFOLIO_MAX_SIZE(PAYLOAD_TOO_LARGE, "EXCEEDED_PORTFOLIO_MAX_SIZE", "첨부 가능한 포트폴리오 최대 용량을 초과했습니다.");
+    INVALID_EXTENSION(BAD_REQUEST, "FILE-1", "유효하지 않은 확장자입니다."),
+    EXCEEDED_PORTFOLIO_MAX_SIZE(PAYLOAD_TOO_LARGE, "FILE-2", "첨부 가능한 포트폴리오 최대 용량을 초과했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
