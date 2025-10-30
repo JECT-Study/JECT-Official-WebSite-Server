7a2d3279912075c0dec8de8a5f592a0f8517d980
023-dev <0_2_3@naver.com>
2025-10-30 23:46:53 +0900

    refactor: Auth 도메인 에러 코드를 문자열 형태로 수정

--- src/main/java/org/ject/support/domain/auth/exception/AuthErrorCode.java
+++ src/main/java/org/ject/support/domain/auth/exception/AuthErrorCode.java
@@ -10,11 +10,11 @@ import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 @Getter
 @AllArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
-    INVALID_AUTH_CODE(UNAUTHORIZED, "INVALID_AUTH_CODE", "인증 번호가 유효하지 않습니다."),
-    NOT_FOUND_AUTH_CODE(UNAUTHORIZED, "NOT_FOUND_AUTH_CODE", "인증 번호를 찾을 수 없습니다."),
-    INVALID_REFRESH_TOKEN(UNAUTHORIZED, "INVALID_REFRESH_TOKEN", "유효하지 않은 리프레시 토큰입니다."),
-    EXPIRED_REFRESH_TOKEN(UNAUTHORIZED, "EXPIRED_REFRESH_TOKEN", "만료된 리프레시 토큰입니다."),
-    INVALID_CREDENTIALS(UNAUTHORIZED, "INVALID_CREDENTIALS", "PIN 번호가 올바르지 않습니다.");
+    INVALID_AUTH_CODE(UNAUTHORIZED, "AUTH-1", "인증 번호가 유효하지 않습니다."),
+    NOT_FOUND_AUTH_CODE(UNAUTHORIZED, "AUTH-2", "인증 번호를 찾을 수 없습니다."),
+    INVALID_REFRESH_TOKEN(UNAUTHORIZED, "AUTH-3", "유효하지 않은 리프레시 토큰입니다."),
+    EXPIRED_REFRESH_TOKEN(UNAUTHORIZED, "AUTH-4", "만료된 리프레시 토큰입니다."),
+    INVALID_CREDENTIALS(UNAUTHORIZED, "AUTH-5", "PIN 번호가 올바르지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
