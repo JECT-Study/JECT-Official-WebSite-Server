238bbfac3d5cb63f8c75614f012552a807cc1140
fad <ojy9406@naver.com>
2025-11-18 18:00:38 +0900

    Revert "feat: Swagger 개발 환경을 위한 CORS 설정 추가"
    This reverts commit 4f12c3c0dbbe091432e4ebac92b8d6f16a9c2470.

--- src/main/java/org/ject/support/common/security/config/SecurityConfig.java
+++ src/main/java/org/ject/support/common/security/config/SecurityConfig.java
@@ -45,9 +45,6 @@ public class SecurityConfig {
     @Value("${security.cors.allowed-origins-client-dev}")
     private String allowedOriginsClientDev;
 
-    @Value("${security.cors.allowed-origins-swagger-dev}")
-    private String allowedOriginsSwaggerDev;
-
     @Bean
     public static RoleHierarchy roleHierarchy() {
         return RoleHierarchyImpl.fromHierarchy("""
@@ -87,7 +84,6 @@ public class SecurityConfig {
         configuration.addAllowedOriginPattern(allowedOrigins);
         configuration.addAllowedOriginPattern(allowedOriginsClient);
         configuration.addAllowedOriginPattern(allowedOriginsClientDev);
-        configuration.addAllowedOriginPattern(allowedOriginsSwaggerDev);
         configuration.addAllowedHeader("*");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowCredentials(true);
