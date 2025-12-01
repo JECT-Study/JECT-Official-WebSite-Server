873568c201d90b6aba1500bbd956b6ca1458246c
fad <ojy9406@naver.com>
2025-12-01 18:38:19 +0900

    chore: JWT 쿠키 생성 로직을 ResponseCookie로 변경 및 SameSite 속성 추가 (#333)
    * chore: JWT 쿠키 생성 로직을 ResponseCookie로 변경 및 SameSite 속성 추가
    * 테스트 코드 수정

--- src/main/java/org/ject/support/common/security/jwt/JwtCookieProvider.java
+++ src/main/java/org/ject/support/common/security/jwt/JwtCookieProvider.java
@@ -1,13 +1,17 @@
 package org.ject.support.common.security.jwt;
 
-import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class JwtCookieProvider {
+
+    private final Environment environment;
 
     @Value("${spring.jwt.token.refresh-expiration-time}")
     private long refreshExpirationTime;
@@ -15,29 +19,39 @@ public class JwtCookieProvider {
     @Value("${security.cors.cookie.domain}")
     private String domain;
 
-    public Cookie createRefreshCookie(String refreshToken) { // TODO: 토큰 생성 로직 통합 및 토큰 정보 enum에서 관리하도록 리팩토링
+    public ResponseCookie createRefreshCookie(String refreshToken) { // TODO: 토큰 생성 로직 통합 및 토큰 정보 enum에서 관리하도록 리팩토링
         String cookieName = "refreshToken";
         return createCookie(cookieName, refreshToken);
     }
 
-    public Cookie createAccessCookie(String accessToken) {
+    public ResponseCookie createAccessCookie(String accessToken) {
         String cookieName = "accessToken";
         return createCookie(cookieName, accessToken);
     }
 
-    public Cookie createVerificationCookie(String verificationToken) {
+    public ResponseCookie createVerificationCookie(String verificationToken) {
         String cookieName = "verificationToken";
         return createCookie(cookieName, verificationToken);
     }
 
-    private Cookie createCookie(String cookieName, String token) {
-        Cookie cookie = new Cookie(cookieName, token);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setDomain(domain);
-        cookie.setPath("/");
-        cookie.setMaxAge((int)(refreshExpirationTime / 1000));
+    private ResponseCookie createCookie(String cookieName, String token) {
+        ResponseCookie.ResponseCookieBuilder builder = ResponseCookie.from(cookieName, token)
+                .domain(domain)
+                .path("/")
+                .maxAge(refreshExpirationTime / 1000)
+                .httpOnly(true)
+                .secure(true);
 
-        return cookie;
+        // 운영은 기본 SameSite=Lax
+        // 개발/QA 환경이면 cross-site 허용
+        if (isDevOrLocal()) {
+            builder.sameSite("None");
+        }
+
+        return builder.build();
+    }
+
+    private boolean isDevOrLocal() {
+        return environment.acceptsProfiles(Profiles.of("dev", "local"));
     }
 }
