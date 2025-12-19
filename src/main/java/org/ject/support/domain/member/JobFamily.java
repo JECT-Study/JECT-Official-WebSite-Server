b16c94fdf6922e11842d629a13226cb8e4dcd6ef
023-dev <0_2_3@naver.com>
2025-12-20 00:26:18 +0900

    feat: JobFamily에 portfolioRequired 필드 추가

--- src/main/java/org/ject/support/domain/member/JobFamily.java
+++ src/main/java/org/ject/support/domain/member/JobFamily.java
@@ -6,11 +6,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum JobFamily {
-    PM("프로덕트 매니저(PM)"),
-    PD("프로덕트 디자이너(PD)"),
-    FE("프론트엔드 개발자(FE)"),
-    BE("백엔드 개발자(BE)"),
+    PM("프로덕트 매니저(PM)", false),
+    PD("프로덕트 디자이너(PD)", true),
+    FE("프론트엔드 개발자(FE)", false),
+    BE("백엔드 개발자(BE)", false),
     ;
 
     private final String description;
+    private final boolean portfolioRequired;
 }
