9a9b1c73d70bd4614e9b9006a1bee66b17bf75b3
fad <ojy9406@naver.com>
2025-12-21 19:03:58 +0900

    feat: 프로젝트 테이블에 badges 컬럼 추가 및 ProjectDetailResponse에 badges 필드 추가 (#360)

--- src/main/java/org/ject/support/domain/project/dto/ProjectDetailResponse.java
+++ src/main/java/org/ject/support/domain/project/dto/ProjectDetailResponse.java
@@ -1,11 +1,12 @@
 package org.ject.support.domain.project.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
-import java.time.LocalDate;
-import java.util.List;
 import lombok.Builder;
 import org.ject.support.domain.member.dto.TeamMemberNames;
 import org.ject.support.domain.project.entity.Project;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Builder
 public record ProjectDetailResponse(
@@ -15,6 +16,7 @@ public record ProjectDetailResponse(
         LocalDate endDate,
         TeamMemberNames teamMemberNames,
         List<String> techStack,
+        List<String> badges,
         String description,
         String serviceUrl,
         List<ProjectIntroResponse> serviceIntros,
@@ -36,6 +38,7 @@ public record ProjectDetailResponse(
                 .endDate(project.getEndDate())
                 .teamMemberNames(teamMemberNames)
                 .techStack(project.getTechStack())
+                .badges(project.getBadges())
                 .description(project.getDescription())
                 .serviceUrl(project.getServiceUrl())
                 .serviceIntros(serviceIntros)
