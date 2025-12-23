5be418f53fe1b76a28aed8bdb445dc2afd6fdfee
fad <ojy9406@naver.com>
2025-12-23 21:22:54 +0900

    feat: 프로젝트 조회 API에서 카테고리 파라미터 nullable 속성 추가

--- src/main/java/org/ject/support/domain/project/repository/ProjectQueryRepository.java
+++ src/main/java/org/ject/support/domain/project/repository/ProjectQueryRepository.java
@@ -5,8 +5,10 @@ import org.ject.support.domain.project.entity.Project;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import javax.annotation.Nullable;
+
 public interface ProjectQueryRepository {
 
-    Page<ProjectResponse> findProjectsByCategory(Project.Category category,
+    Page<ProjectResponse> findProjectsByCategory(@Nullable Project.Category category,
                                                  Pageable pageable);
 }
