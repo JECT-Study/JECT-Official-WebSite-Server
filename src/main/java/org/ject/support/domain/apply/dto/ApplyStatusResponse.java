1cd4659545af46023a48c7c8f56286ab3336d0c1
023-dev <0_2_3@naver.com>
2025-12-29 04:10:19 +0900

    refactor: ApplyStatusResponse에서 step 필드 및 관련 팩토리 메서드 제거

--- src/main/java/org/ject/support/domain/apply/dto/ApplyStatusResponse.java
+++ src/main/java/org/ject/support/domain/apply/dto/ApplyStatusResponse.java
@@ -1,25 +1,12 @@
 package org.ject.support.domain.apply.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import org.ject.support.domain.apply.domain.Apply;
 import org.ject.support.domain.apply.domain.Apply.Status;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApplyStatusResponse(
-        Status status,
-        String step
+        Status status
 ) {
-
-    public static ApplyStatusResponse of(Status status) {
-        return new ApplyStatusResponse(status, null);
-    }
-
-    // 프로필 작성을 하지 않았을 경우
-    public static ApplyStatusResponse tempSavedProfile() {
-        return new ApplyStatusResponse(Status.TEMP_SAVED, "PROFILE");
-    }
-
-    // 프로필 작성 이후, 지원서 작성 중인 경우
-    public static ApplyStatusResponse tempSavedApply() {
-        return new ApplyStatusResponse(Status.TEMP_SAVED, "APPLY");
+    public static ApplyStatusResponse of(Apply apply) {
+        return new ApplyStatusResponse(apply.getStatus());
     }
 }
