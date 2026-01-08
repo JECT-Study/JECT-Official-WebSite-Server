f5f65bcd63374e8eb5f3a97affbf5de7658c6fd4
023-dev <0_2_3@naver.com>
2026-01-08 20:54:55 +0900

    feat: ContentType enum 추가하여 콘텐츠 유형 정의

---
+++ src/main/java/org/ject/support/domain/jectalk/enums/ContentType.java
@@ -0,0 +1,13 @@
+package org.ject.support.domain.jectalk.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ContentType {
+    YOUTUBE("YouTube"),
+    ;
+
+    private final String name;
+}
