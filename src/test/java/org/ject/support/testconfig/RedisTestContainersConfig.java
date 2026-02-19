152b3e253ad7dae9c918b0af95952859b8567568
023-dev <0_2_3@naver.com>
2026-02-19 23:59:24 +0900

    style: Unused import 및 어노테이션 정리

--- src/test/java/org/ject/support/testconfig/RedisTestContainersConfig.java
+++ src/test/java/org/ject/support/testconfig/RedisTestContainersConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
