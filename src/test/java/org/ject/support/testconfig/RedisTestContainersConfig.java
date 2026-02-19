5242f3696545d702618e0aedaa3d6c0bd8006e75
023-dev <0_2_3@naver.com>
2026-02-20 00:08:12 +0900

    style: RedisTestContainersConfig 잔여 Unused import 정리

--- src/test/java/org/ject/support/testconfig/RedisTestContainersConfig.java
+++ src/test/java/org/ject/support/testconfig/RedisTestContainersConfig.java
@@ -2,16 +2,7 @@ package org.ject.support.testconfig;
 
 import java.time.Duration;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
