1fca5da856d6905bc5a6be20634d7265d781914d
023-dev <0_2_3@naver.com>
2026-02-20 00:06:30 +0900

    style: MysqlTestContainersConfig Unused import 정리

--- src/test/java/org/ject/support/testconfig/MysqlTestContainersConfig.java
+++ src/test/java/org/ject/support/testconfig/MysqlTestContainersConfig.java
@@ -1,11 +1,8 @@
 package org.ject.support.testconfig;
 
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Profile;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Profile("test")
 @TestConfiguration
