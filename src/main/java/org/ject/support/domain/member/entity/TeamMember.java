ee39c8f96ed992482ccdfe594ba0cbbd13b1a746
023-dev <0_2_3@naver.com>
2026-02-20 23:49:16 +0900

    feat: TeamMember에 jobFamily 필드 추가
    - TeamMember 엔티티에 jobFamily 필드 추가 (@Enumerated, @Column)
    - 클래스 레벨에 @Getter 어노테이션 추가

--- src/main/java/org/ject/support/domain/member/entity/TeamMember.java
+++ src/main/java/org/ject/support/domain/member/entity/TeamMember.java
@@ -1,6 +1,9 @@
 package org.ject.support.domain.member.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,10 +13,13 @@ import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.ject.support.domain.base.BaseTimeEntity;
+import org.ject.support.domain.member.JobFamily;
 
 @Entity
+@Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -30,4 +36,8 @@ public class TeamMember extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id", nullable = false)
     private Team team;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "varchar(45)")
+    private JobFamily jobFamily;
 }
