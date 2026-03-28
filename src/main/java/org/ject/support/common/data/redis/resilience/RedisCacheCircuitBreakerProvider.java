b3355b44eff40c0e8585f1e15d3ef85eb8892b54
023-dev <0_2_3@naver.com>
2026-03-28 18:28:28 +0900

    refactor: remove unnecessary comment in RedisCacheCircuitBreakerProvider

--- src/main/java/org/ject/support/common/data/redis/resilience/RedisCacheCircuitBreakerProvider.java
+++ src/main/java/org/ject/support/common/data/redis/resilience/RedisCacheCircuitBreakerProvider.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RedisCacheCircuitBreakerProvider {
 
-    // 이름 기반의 서킷 브레이커 인스턴스를 관리하고 재사용하는 전역 레지스트리
     private final CircuitBreakerRegistry circuitBreakerRegistry;
 
     public CircuitBreaker get(final String cacheName) {
