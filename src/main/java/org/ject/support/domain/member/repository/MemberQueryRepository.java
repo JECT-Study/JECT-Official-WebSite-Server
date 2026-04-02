9581895ee08f0843892ce77b8f79773209bf7768
023-dev <0_2_3@naver.com>
2026-04-02 22:38:16 +0900

    refactor: `MemberQueryRepository` 및 `MemberQueryRepositoryImpl` 리팩토링
    `MemberQueryRepository`에서 사용하지 않는 메서드를 제거하고,
    `MemberQueryRepositoryImpl`에서 불필요한 임포트를 정리했습니다.

--- src/main/java/org/ject/support/domain/member/repository/MemberQueryRepository.java
+++ src/main/java/org/ject/support/domain/member/repository/MemberQueryRepository.java
@@ -1,19 +1,11 @@
 package org.ject.support.domain.member.repository;
 
-import org.ject.support.domain.member.JobFamily;
-import org.ject.support.domain.member.Role;
-import org.ject.support.domain.member.dto.MemberProjection;
-import org.ject.support.domain.member.dto.TeamMemberNames;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
 import java.util.List;
+import org.ject.support.domain.member.dto.TeamMemberNames;
 
 public interface MemberQueryRepository {
 
     TeamMemberNames findMemberNamesByTeamId(Long teamId);
 
     List<String> findEmailsByIdsAndNotSubmitted(List<Long> applicantIds);
-
-    Page<MemberProjection> findMembers(final Role role, final JobFamily jobFamily, final Long semesterId, final Pageable pageable);
 }
