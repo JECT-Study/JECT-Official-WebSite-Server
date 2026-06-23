54b55131871801dc374dec00bffdfce51b4065ef
kangkigs <dlflq11@gmail.com>
2026-06-23 15:03:02 +0900

    [feat] 일반 구성원 추가 dto 검증 조건 추가

--- src/main/java/org/ject/support/admin/member/dto/request/CreateMemberSemesterRequest.java
+++ src/main/java/org/ject/support/admin/member/dto/request/CreateMemberSemesterRequest.java
@@ -21,6 +21,7 @@ public record CreateMemberSemesterRequest(
 	선택 항목: 팀id, 비고, 관심도메인, 거주지역, 직무 관련 경험
 	 */
 	@NotBlank(message = "이름을 입력해주세요")
+	@Size(max = 20, message = "이름은 20자 이하로 입력해주세요.")
 	@Pattern(regexp = "^[^\\s]+$", message = "이름은 공백없이 입력해주세요")
 	String name,
 
@@ -30,6 +31,7 @@ public record CreateMemberSemesterRequest(
 	String email,
 
 	@NotBlank(message = "전화번호를 입력해주세요")
+	@Pattern(regexp = "^010\\d{8}$", message = "010으로 시작하는 11자리 숫자를 입력해주세요.")
 	String phoneNumber,
 
 	@NotNull(message = "포지션을 선택해주세요")
@@ -48,6 +50,7 @@ public record CreateMemberSemesterRequest(
 
 	ExperiencePeriod experiencePeriod,
 
+	@Size(max = 100, message = "비고는 100자 이하로 입력해주세요.")
 	String memo,
 
 	@Size(min = 1, max = 3, message = "관심 도메인은 최소 1개부터 최대 3개까지 선택 가능합니다.")
