7e52a28ecaae63aca9e78dfeb72be4309abaeeb6
kangkigs <dlflq11@gmail.com>
2026-06-30 02:09:53 +0900

    [feat] 일반 구성원 목록 조회 응답 구조 추가

---
+++ src/main/java/org/ject/support/admin/member/dto/projection/SearchMemberSemesterProjection.java
@@ -0,0 +1,15 @@
+package org.ject.support.admin.member.dto.projection;
+
+import org.ject.support.domain.member.CareerDetails;
+import org.ject.support.domain.member.ExperiencePeriod;
+import org.ject.support.domain.member.JobFamily;
+
+public record SearchMemberSemesterProjection(
+	Long memberActivityId,
+	String name,
+	JobFamily jobFamily,
+	String phoneNumber,
+	CareerDetails careerDetails,
+	ExperiencePeriod experiencePeriod
+) {
+}
