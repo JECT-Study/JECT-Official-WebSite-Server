582a377e68e88bae421f9e2324aa397068f40e60
kangkigs <dlflq11@gmail.com>
2026-07-22 17:27:36 +0900

    [feat] 메이커스팀 구성원 상세 조회 DTO 추가

---
+++ src/main/java/org/ject/support/admin/member/dto/projection/MemberMakersDetailProjection.java
@@ -0,0 +1,59 @@
+package org.ject.support.admin.member.dto.projection;
+
+import java.util.List;
+
+import org.ject.support.domain.member.ActivityStatus;
+import org.ject.support.domain.member.Availability;
+import org.ject.support.domain.member.CareerDetails;
+import org.ject.support.domain.member.CareerLevel;
+import org.ject.support.domain.member.ExperiencePeriod;
+import org.ject.support.domain.member.JobFamily;
+import org.ject.support.domain.member.MakersTeam;
+import org.ject.support.domain.member.Region;
+import org.ject.support.domain.recruit.domain.RecruitTypeDetail;
+
+public record MemberMakersDetailProjection(
+	Long id,
+
+	String name,
+
+	String email,
+
+	String phoneNumber,
+
+	JobFamily jobFamily,
+
+	CareerDetails careerDetails,
+
+	MakersTeam makersTeam,
+
+	RecruitTypeDetail recruitTypeDetail,
+
+	Region region,
+
+	List<String> interestedDomains,
+
+	ExperiencePeriod experiencePeriod,
+
+	Availability mentoringAvailability,
+
+	Availability projectSupplementAvailability,
+
+	Availability speakerAvailability,
+
+	CareerLevel careerLevel,
+
+	ActivityStatus activityStatus,
+
+	String skills,
+
+	String company,
+
+	String expertTopics,
+
+	String activityCertNumber,
+
+	String memo
+
+) {
+}
---
+++ src/main/java/org/ject/support/admin/member/dto/response/MemberMakersDetailResponse.java
@@ -0,0 +1,85 @@
+package org.ject.support.admin.member.dto.response;
+
+import java.util.List;
+
+import org.ject.support.admin.member.dto.projection.MemberMakersDetailProjection;
+import org.ject.support.domain.member.ActivityStatus;
+import org.ject.support.domain.member.Availability;
+import org.ject.support.domain.member.CareerDetails;
+import org.ject.support.domain.member.CareerLevel;
+import org.ject.support.domain.member.ExperiencePeriod;
+import org.ject.support.domain.member.JobFamily;
+import org.ject.support.domain.member.MakersTeam;
+import org.ject.support.domain.member.Region;
+import org.ject.support.domain.recruit.domain.RecruitTypeDetail;
+
+public record MemberMakersDetailResponse(
+	Long id,
+
+	String name,
+
+	String email,
+
+	String phoneNumber,
+
+	JobFamily jobFamily,
+
+	CareerDetails careerDetails,
+
+	MakersTeam makersTeam,
+
+	RecruitTypeDetail recruitTypeDetail,
+
+	Region region,
+
+	List<String> interestedDomains,
+
+	ExperiencePeriod experiencePeriod,
+
+	Availability mentoringAvailability,
+
+	Availability projectSupplementAvailability,
+
+	Availability speakerAvailability,
+
+	CareerLevel careerLevel,
+
+	ActivityStatus activityStatus,
+
+	String skills,
+
+	String company,
+
+	String expertTopics,
+
+	String activityCertNumber,
+
+	String memo
+
+) {
+	public static MemberMakersDetailResponse from(MemberMakersDetailProjection projection) {
+		return new MemberMakersDetailResponse(
+			projection.id(),
+			projection.name(),
+			projection.email(),
+			projection.phoneNumber(),
+			projection.jobFamily(),
+			projection.careerDetails(),
+			projection.makersTeam(),
+			projection.recruitTypeDetail(),
+			projection.region(),
+			projection.interestedDomains(),
+			projection.experiencePeriod(),
+			projection.mentoringAvailability(),
+			projection.projectSupplementAvailability(),
+			projection.speakerAvailability(),
+			projection.careerLevel(),
+			projection.activityStatus(),
+			projection.skills(),
+			projection.company(),
+			projection.expertTopics(),
+			projection.activityCertNumber(),
+			projection.memo()
+		);
+	}
+}
