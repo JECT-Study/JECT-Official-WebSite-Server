11254f1dae66ea2bbf2974c29a4bd618519b8276
kangkigs <dlflq11@gmail.com>
2026-08-17 14:48:22 +0900

    test: MemberService 테스트 보강

--- src/test/java/org/ject/support/admin/member/service/AdminMemberServiceTest.java
+++ src/test/java/org/ject/support/admin/member/service/AdminMemberServiceTest.java
@@ -14,6 +14,8 @@ import org.ject.support.domain.member.ExperiencePeriod;
 import org.ject.support.domain.member.JobFamily;
 import org.ject.support.domain.member.Region;
 import org.ject.support.domain.member.entity.Member;
+import org.ject.support.domain.member.exception.MemberErrorCode;
+import org.ject.support.domain.member.exception.MemberException;
 import org.ject.support.domain.member.repository.MemberActivityRepository;
 import org.ject.support.domain.member.repository.MemberRepository;
 import org.ject.support.domain.recruit.domain.RecruitTypeDetail;
@@ -165,6 +167,22 @@ class AdminMemberServiceTest {
 	}
 
 	@Test
+	@DisplayName("남은 활동은 없지만 구성원을 찾을 수 없으면 삭제에 실패한다")
+	void 남은_활동은_없지만_구성원을_찾을_수_없으면_삭제에_실패한다() {
+		// given
+		Long memberId = 1L;
+		given(memberActivityRepository.existsByMemberId(memberId)).willReturn(false);
+		given(memberRepository.findById(memberId)).willReturn(Optional.empty());
+
+		// when & then
+		assertThatThrownBy(() -> adminMemberService.deleteMemberIfNoActivity(memberId))
+			.isInstanceOf(MemberException.class)
+			.extracting("errorCode")
+			.isEqualTo(MemberErrorCode.NOT_FOUND_MEMBER);
+		verify(memberRepository, never()).delete(any(Member.class));
+	}
+
+	@Test
 	@DisplayName("다른 활동이 남아 있으면 구성원을 유지한다")
 	void 다른_활동이_남아_있으면_구성원을_유지한다() {
 		// given
@@ -198,6 +216,23 @@ class AdminMemberServiceTest {
 			return true;
 		}));
 		verify(memberRepository).deleteAll(List.of(first, third));
+	}
+
+	@Test
+	@DisplayName("일괄 삭제할 구성원 일부를 찾을 수 없으면 모두 삭제하지 않는다")
+	void 일괄_삭제할_구성원_일부를_찾을_수_없으면_모두_삭제하지_않는다() {
+		// given
+		Set<Long> memberIds = Set.of(1L, 2L);
+		Member member = mock(Member.class);
+		given(memberActivityRepository.findMemberIdsWithActivity(memberIds)).willReturn(Set.of());
+		given(memberRepository.findAllById(any())).willReturn(List.of(member));
+
+		// when & then
+		assertThatThrownBy(() -> adminMemberService.deleteMembersIfNoActivity(memberIds))
+			.isInstanceOf(MemberException.class)
+			.extracting("errorCode")
+			.isEqualTo(MemberErrorCode.NOT_FOUND_MEMBER);
+		verify(memberRepository, never()).deleteAll(any());
 	}
 
 	@Test
