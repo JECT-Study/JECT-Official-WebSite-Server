10022e0416642ebdd1344f205e68e5afda4167ba
023-dev <rhdgmltkd88@gmail.com>
2026-08-29 14:59:16 +0900

    style: 발송 대상 테스트 설명 보완

--- src/test/java/org/ject/support/admin/mail/domain/MailDispatchTargetTest.java
+++ src/test/java/org/ject/support/admin/mail/domain/MailDispatchTargetTest.java
@@ -4,11 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.ject.support.admin.mail.exception.MailException;
 
 class MailDispatchTargetTest {
 
     @Test
+    @DisplayName("발송 대상을 생성하면 대기 상태와 이메일 snapshot이 저장된다")
     void 발송_대상을_생성하면_대기_상태와_이메일_snapshot이_저장된다() {
         // given
         MailDispatchJob job = MailDispatchJob.create(1L, 2L, 3L, "제목", "본문", "{}", 1);
@@ -24,6 +26,7 @@ class MailDispatchTargetTest {
     }
 
     @Test
+    @DisplayName("발송 대상을 성공 상태로 변경한다")
     void 발송_대상을_성공_상태로_변경한다() {
         // given
         MailDispatchTarget target = MailDispatchTarget.pending(
@@ -41,6 +44,7 @@ class MailDispatchTargetTest {
     }
 
     @Test
+    @DisplayName("발송 대상을 실패 상태와 실패 사유로 변경한다")
     void 발송_대상을_실패_상태와_실패_사유로_변경한다() {
         // given
         MailDispatchTarget target = MailDispatchTarget.pending(
@@ -58,6 +62,7 @@ class MailDispatchTargetTest {
     }
 
     @Test
+    @DisplayName("처리된 발송 대상은 다시 처리할 수 없다")
     void 처리된_발송_대상은_다시_처리할_수_없다() {
         // given
         MailDispatchTarget target = MailDispatchTarget.pending(
